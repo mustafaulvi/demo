8e623384dfaf3a3a17fc5fd169175bbabf6466e6
mustafaulvi <ulvimustafa@gmail.com>
2023-05-31 21:29:41 +0200

    SwaggerConfig V2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Hallo Wold Project",
+				version = "1.0.0"
+
+		)
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/configuration/SwaggerConfig.java
+++ src/main/java/com/example/demo/configuration/SwaggerConfig.java
@@ -1,41 +1,25 @@
+/*
 package com.example.demo.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Set;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableSwagger2
 @RequiredArgsConstructor
-public class SwaggerConfig {
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hallo Wold Project",
+                version = "1.0.0"
+
+        )
+)
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private final ApplicationProperties applicationProperties;
 
-    @Bean
-    public Docket createDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .protocols(Set.of("https", "http"))
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(applicationProperties.getBasePackage()))
-                .build();
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title(applicationProperties.getTitle())
-                .description(applicationProperties.getDescription())
-                .termsOfServiceUrl(applicationProperties.getTerms())
-                .version(applicationProperties.getVersion())
-                .build();
-    }
 }
+*/
